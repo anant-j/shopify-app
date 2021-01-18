aa03b9664eed456fc55659967ffd8af1babaec72
Anant Jain <anant.j2409@gmail.com>
2021-01-17 23:08:03 -0500

    Adding comments

--- Back-End/firebase_handler.py
+++ Back-End/firebase_handler.py
@@ -5,11 +5,12 @@ cred = credentials.Certificate('cred.json')
 default_app = initialize_app(cred)
 db = firestore.client()
 
-repo = cred.project_id+".appspot.com"
+repo = cred.project_id + ".appspot.com"
 client = storage.Client.from_service_account_json('cred.json')
 bucket = client.get_bucket(repo)
 
 
+# Get user's statistics
 def getUserStats(email):
     doc_ref = db.collection(u'users').document(email)
     doc = doc_ref.get()
@@ -18,6 +19,7 @@ def getUserStats(email):
     return ({})
 
 
+# Get user's data
 def getAllUserData(email):
     doc_ref = db.collection(u'users').document(email)
     doc = doc_ref.get()
@@ -26,16 +28,20 @@ def getAllUserData(email):
     return ({})
 
 
+# Check if the image already exists in storage
 def blob_exists(filename):
     blob = bucket.blob(filename)
     return blob.exists()
 
 
-def set_db(email, data):
+# Updates the database for given user with the provided data
+def update_db(email, data):
     db.collection(u'users').document(email).set(data)
     return("DONE")
 
 
+# Get a list of url's for images whose metadata's properties match the
+# given data
 def list_img_urls_where(metadata, data):
     results_list = []
     for blob in client.list_blobs(repo):
@@ -45,20 +51,16 @@ def list_img_urls_where(metadata, data):
     return results_list
 
 
+# Get the blob for provided url
 def get_blob(url):
     return (bucket.blob(url))
 
 
+# Returns a list of all blobs in storage
 def get_blobs_list():
     return (client.list_blobs(repo))
 
 
+# Upload file to storage for specified blob
 def upload_to_storage(blob, file):
     blob.upload_from_file(file, client=client)
-
-
-def print_cred():
-    return("Dones")
-
-
-print_cred()
--- Back-End/main.py
+++ Back-End/main.py
@@ -1,3 +1,4 @@
+# All Imports
 import os
 import json
 import random
@@ -16,31 +17,36 @@ with open(my_directory + '/secrets.json', 'r') as myfile:
     data = myfile.read()
 obj = json.loads(data)
 
+# Setting up application
 app = Flask(__name__)
-app.debug = True
+app.debug = False
+CORS(app, support_credentials=True)
+
+# Setting up Discord OAuth
 app.secret_key = obj["OAUTH2_CLIENT_SECRET"]
 app.config["DISCORD_CLIENT_ID"] = obj["OAUTH2_CLIENT_ID"]
 app.config["DISCORD_CLIENT_SECRET"] = obj["OAUTH2_CLIENT_SECRET"]
 app.config["DISCORD_REDIRECT_URI"] = obj["Callback"]
-# app.config['MAX_CONTENT_LENGTH'] = 8 * 1024 * 1024
-app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif', '.jpeg']
-websiteAddress = obj["WebAddress"]
-redirectAddress = obj["RedirectAddress"]
-
 API_BASE_URL = os.environ.get('API_BASE_URL', 'https://discord.com/api')
 AUTHORIZATION_BASE_URL = API_BASE_URL + '/oauth2/authorize'
 TOKEN_URL = API_BASE_URL + '/oauth2/token'
-
-CORS(app, support_credentials=True)
-
 if 'http://' in app.config["DISCORD_REDIRECT_URI"]:
     os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = 'true'
 
+# Setting up Image criteria
+app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif', '.jpeg']
 
+# Setting up API, Webapp & CORS criteria
+websiteAddress = obj["WebAddress"]
+redirectAddress = obj["RedirectAddress"]
+
+
+# This function updates the Discord Oauth Tokens
 def token_updater(token):
     session['oauth2_token'] = token
 
 
+# Method to make a Discord OAuth session
 def make_session(token=None, state=None, scope=None):
     return OAuth2Session(
         client_id=app.config["DISCORD_CLIENT_ID"],
@@ -59,11 +65,14 @@ def make_session(token=None, state=None, scope=None):
 discord = DiscordOAuth2Session(app)
 
 
+# Redirect to webpage if API is called directly
 @app.route('/')
 def noapi():
     return redirect(redirectAddress)
 
 
+# Login endpoint
+# Returns the login url that the webpage is redirected to.
 @app.route("/login")
 @cross_origin(supports_credentials=True,
               allow_headers=['Content-Type',
@@ -80,6 +89,8 @@ def login():
     return (authorization_url)
 
 
+# Callback endpoint
+# Redirects to the webpage when called.
 @app.route("/callback")
 @cross_origin(supports_credentials=True)
 def callback():
@@ -90,6 +101,8 @@ def callback():
         return redirect(redirectAddress)
 
 
+# Userdata endpoint
+# Returns users email, and upload statistics.
 @app.route("/userdata")
 @cross_origin(supports_credentials=True,
               allow_headers=['Content-Type',
@@ -98,18 +111,21 @@ def callback():
 @requires_authorization
 def userData():
     try:
-        if(discord.authorized):
-            user = discord.fetch_user()
-            email = user.email
+        if(discord.authorized):  # If user is authorized/logged in
+            email = discord.fetch_user().email  # Get user's email address
             data = storage.getUserStats(email)
             resp = make_response(jsonify(email=email, stats=data))
             return resp
         else:
+            # User is not logged in, or is unauthorized
             return ("Unauthorized", 401)
     except Exception as e:
         return (str(e))
 
 
+# userimages endpoint
+# Returns a comma seperated string containing links to all images uploaded
+# by the user
 @app.route("/userimages")
 @cross_origin(supports_credentials=True,
               allow_headers=['Content-Type',
@@ -118,27 +134,34 @@ def userData():
 @requires_authorization
 def userImages():
     try:
-        if(discord.authorized):
-            user = discord.fetch_user()
-            email = user.email
+        if(discord.authorized):  # If user is authorized/logged in
+            email = discord.fetch_user().email  # Get user's email address
             result = storage.list_img_urls_where("uploader", email)
             return (",".join(result))
         else:
+            # User is not logged in, or is unauthorized
             return ("Unauthorized", 401)
     except Exception as e:
         return (str(e))
 
 
+# get_all_sfw endpoint
+# Returns a comma seperated string containing links to all SFW images
 @app.route("/get_all_sfw")
 @cross_origin(supports_credentials=True,
               allow_headers=['Content-Type',
                              'Access-Control-Allow-Origin'],
               origins=websiteAddress)
 def get_sfw_images():
-    result = storage.list_img_urls_where("type", "SFW")
-    return (",".join(result))
+    try:
+        result = storage.list_img_urls_where("type", "SFW")
+        return (",".join(result))
+    except Exception as e:
+        return (str(e))
 
 
+# get_all_nsfw endpoint
+# Returns a comma seperated string containing links to all NSSFW images
 @app.route("/get_all_nsfw")
 @cross_origin(supports_credentials=True,
               allow_headers=['Content-Type',
@@ -147,15 +170,18 @@ def get_sfw_images():
 @requires_authorization
 def get_nsfw_images():
     try:
-        if(discord.authorized):
+        if(discord.authorized):  # If user is authorized/logged in
             result = storage.list_img_urls_where("type", "NSFW")
             return (",".join(result))
         else:
+            # User is not logged in, or is unauthorized
             return ("Unauthorized", 401)
     except Exception as e:
         return (str(e))
 
 
+# upload endpoint
+# Redirects to "my" webpage on success, otherwise error status is returned.
 @app.route("/upload", methods=['POST'])
 @cross_origin(supports_credentials=True,
               allow_headers=['Content-Type',
@@ -164,60 +190,81 @@ def get_nsfw_images():
 @requires_authorization
 def uploadimg():
     try:
-        if(discord.authorized):
-            user = discord.fetch_user()
-            email = user.email
+        if(discord.authorized):  # If user is authorized/logged in
+            email = discord.fetch_user().email  # Get user's email address
+            # Get the uploaded file
             uploaded_file = request.files["uploadedfile"]
+            # Get the uploaded file's name
             original_filename = secure_filename(uploaded_file.filename)
-            filename = random_name_gen()
+            # Generate a random (32 byte) filename
+            filename = random_name_gen(32)
+            # If given filename is already used,
+            # regenerate until a unique one is generated
             while (storage.blob_exists(filename)):
-                filename = random_name_gen()
+                filename = random_name_gen(32)
+            # Validate file extensions
             if original_filename != '':
                 file_ext = os.path.splitext(original_filename)[1]
                 if file_ext.lower() not in app.config['UPLOAD_EXTENSIONS']:
                     return "Invalid image", 400
                 else:
+                    # Add extension to 32 byte file name
                     filename = filename + file_ext
+            # Create a temporary blob
             tempblob = storage.get_blob("temp/" + filename)
+            # Upload file to temporary blob
             storage.upload_to_storage(tempblob, uploaded_file)
-            tempblob.make_public()
+            tempblob.make_public()  # Make temporary blob publicly accessible
+            # Get temporary blob's URL, send it through Clarifai's API
             NSFW = clarifai.is_NSFW(tempblob.public_url)
-            tempblob.delete()
+            tempblob.delete()  # Delete temporary blob
+            # Set file pointer to initial index
             uploaded_file.seek(0)
-            data = storage.getAllUserData(email)
+            data = storage.getAllUserData(email)  # Get all User's data
+            # If there are no stats for the user, set up the "stats" dictionary
             if("stats" not in data):
                 data["stats"] = {}
                 data["stats"]["total"] = 0
                 data["stats"]["nsfw_count"] = 0
                 data["stats"]["sfw_count"] = 0
+            # If there is no "images" array for the user, set up array
             if("images" not in data):
                 data["images"] = []
-            blob = storage.get_blob(filename)
+            blob = storage.get_blob(filename)  # Create blob for file
+            # Upload file to the blob
             storage.upload_to_storage(blob, uploaded_file)
-            blob.make_public()
-            if(NSFW):
+            blob.make_public()  # Make blob publicly accessible
+            if(NSFW):  # If image is NSFW
+                # Update metadata
                 metadata = {'type': 'NSFW', 'uploader': email,
                             "filename": original_filename}
                 blob.metadata = metadata
                 data["stats"]["total"] += 1
                 data["stats"]["nsfw_count"] += 1
-            else:
+            else:  # If image is SFW
+                # Update metadata
                 metadata = {'type': 'SFW', 'uploader': email,
                             "filename": original_filename}
                 blob.metadata = metadata
                 data["stats"]["total"] += 1
                 data["stats"]["sfw_count"] += 1
+            # Add images to user's "images" array
             data["images"].append(filename)
+            # Set blob's content type
             blob.content_type = "image/" + file_ext[1:]
-            blob.patch()
-            storage.set_db(email, data)
+            blob.patch()  # Update blob's metadata
+            # Update user's statistics and image array in firebase
+            storage.update_db(email, data)
             return redirect(redirectAddress + "/my")
         else:
+            # User is not logged in, or is unauthorized
             return ("Unauthorized", 401)
     except Exception as e:
         return (str(e))
 
 
+# delete endpoint
+# Redirects to "my" webpage on success, otherwise error status is returned
 @app.route("/delete")
 @cross_origin(supports_credentials=True,
               allow_headers=['Content-Type',
@@ -226,29 +273,36 @@ def uploadimg():
 @requires_authorization
 def delete_image():
     try:
-        if(discord.authorized):
-            user = discord.fetch_user()
-            email = user.email
-            imageid = request.args.get('id')
-            data = storage.getAllUserData(email)
+        if(discord.authorized):  # If user is authorized/logged in
+            email = discord.fetch_user().email  # Get user's email address
+            imageid = request.args.get('id')  # Get requested image's id
+            data = storage.getAllUserData(email)  # Get all User's data
+            # For every blob in all images
             for blob in storage.get_blobs_list():
+                # If image uploader is the same as requester, and
+                # image name is the same as requested
                 if (blob.metadata["uploader"] ==
                         email and blob.name == imageid):
-                    if(blob.metadata["type"] == "NSFW"):
+                    if(blob.metadata["type"] == "NSFW"):  # If image is NSFW
                         data["stats"]["nsfw_count"] -= 1
-                    else:
+                    else:  # If image is SFW
                         data["stats"]["sfw_count"] -= 1
                     data["stats"]["total"] -= 1
+                    # Remove image from "images" array for user
                     data["images"].remove(imageid)
-                    blob.delete()
-            storage.set_db(email, data)
+                    blob.delete()  # Delete image
+            # Set storage in firebase with updated data
+            storage.update_db(email, data)
             return (str(redirectAddress + "/my"))
         else:
+            # User is not logged in, or is unauthorized
             return ("Unauthorized", 401)
     except Exception as e:
         return (str(e))
 
 
+# logout endpoint
+# Logs out user
 @app.route('/logout')
 @cross_origin(supports_credentials=True,
               allow_headers=['Content-Type',
@@ -262,24 +316,30 @@ def logout():
     return (resp)
 
 
+# 404 error handler
 @app.errorhandler(404)
 def page_not_found(e):
     return (redirect(redirectAddress))
 
 
+# 500 error handler
 @app.errorhandler(500)
 def internal_server_error(e):
     return (redirect(redirectAddress))
 
 
-def random_name_gen():
+# Function to generate a random name consisting of
+# Uppercase letters (A-Z)
+# Lowecase letters (a-z)
+# Numbers/Digits (0-9)
+def random_name_gen(size):
     return (
         ''.join(
             random.choices(
                 string.ascii_uppercase +
                 string.digits +
                 string.ascii_lowercase,
-                k=32)))
+                k=size)))
 
 
 if __name__ == '__main__':
